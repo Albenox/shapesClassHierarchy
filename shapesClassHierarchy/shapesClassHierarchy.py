2dbc20490969f59a646b7d611cef63d4fde1438b
Gabriel Inocentes <gabriel.inocentes@gmail.com>
2026-04-23 21:46:50 -0400

    Added rectangle child function, and also fixed massive oversight where the child functions were being made inside the parent function, BasicShape

--- shapesClassHierarchy/shapesClassHierarchy.py
+++ shapesClassHierarchy/shapesClassHierarchy.py
@@ -42,40 +42,82 @@ class BasicShape(ABC):
     def calc_area(self):
         pass
 
-    # A child class that inherits from the BasicShape class, and represents a circle, with properties for the center coordinates and radius, and a method to calculate the area of the circle
-    class Circle(BasicShape):
-        def __init__(self, x, y, r, n = "Circle"):
-            # Calls the __init__ method of the parent class to initialize the area and name properties
-            super().__init__() 
+# A child class that inherits from the BasicShape class, and represents a circle, with properties for the center coordinates and radius, and a method to calculate the area of the circle
+class Circle(BasicShape):
+    def __init__(self, x, y, r, n = "Circle"):
+        # Calls the __init__ method of the parent class to initialize the area and name properties
+        super().__init__() 
             
-            self._x_center = x  
-            self._y_center = y
-            self._radius = r
-            self.name = n
+        self._x_center = x  
+        self._y_center = y
+        self._radius = r
+        self.name = n
 
-            # Calls the calc_area method to calculate the area of the circle when the object is created
+        # Calls the calc_area method to calculate the area of the circle when the object is created
+        self.calc_area() 
+
+    # Property for radius, x_center, and y_center that waits to see if .radius, .x_center, or .y_center is called, and if it is, returns the value of the radius, x_center, or y_center,
+    @property
+    def radius(self):
+        return self._radius
+
+    @property
+    def x_center(self):
+        return self._x_center
+
+    @property
+    def y_center(self):
+        return self._y_center
+
+    # Setter for radius, that also recalculates the area when called, and checks for valid input
+    @radius.setter
+    def radius(self, value):
+        if value >= 0:
+            self._radius = value
+            # Calls to calc_area to recalculate the area of the circle when the radius is changed
             self.calc_area() 
 
-        # Property for radius, x_center, and y_center that waits to see if .radius, .x_center, or .y_center is called, and if it is, returns the value of the radius, x_center, or y_center,
-        @property
-        def radius(self):
-            return self._radius
+    def calc_area(self):
+        self._area = math.pi * self._radius ** 2
 
-        @property
-        def x_center(self):
-            return self._x_center
-
-        @property
-        def y_center(self):
-            return self._y_center
-
-        # Setter for radius, that also recalculates the area when called, and checks for valid input
-        @radius.setter
-        def radius(self, value):
-            if value >= 0:
-                self._radius = value
-                # Calls to calc_area to recalculate the area of the circle when the radius is changed
-                self.calc_area() 
-
-        def calc_area(self):
-            self._area = math.pi * self._radius ** 2
+# A child class that inherits from the BasicShape class, and represents a rectangle, with properties for length and width, and a method to calculate the area of the rectangle
+class Rectangle(BasicShape):
+    def __init__(self, l, w, n = "Rectangle"):
+        # Calls the __init__ method of the parent class to initialize the area and name properties
+        super().__init__() 
+            
+        # Protected instance variables for length and width, with placeholder blank values so they can still be used without error of not being initialized
+        self._length = l
+        self._width = w
+        self.name = n
+            
+        # Calls the calc_area method to calculate the area of the rectangle when the object is created
+        self.calc_area() 
+        
+    # Properties for length and width that wait to see if .length or .width is called, and if it is, returns the value of the length or width
+    @property
+    def length(self):
+        return self._length
+            
+    @property
+    def width(self):
+        return self._width
+        
+    # Setters for length and width that also recalculate the area when called, and checks for valid input
+    @length.setter
+    def length(self, value):
+        if value >= 0:
+            self._length = value
+            # Calls to calc_area to recalculate the area of the rectangle when the length is changed
+            self.calc_area()
+        
+    @width.setter
+    def width(self, value):
+        if value >= 0:
+            self._width = value
+            # Calls to calc_area to recalculate the area of the rectangle when the width is changed
+            self.calc_area() 
+         
+    # A method that calculates the area of the rectangle using the formula length * width, and sets the value of the area property to the calculated area
+    def calc_area(self):
+        self._area = self._length * self._width

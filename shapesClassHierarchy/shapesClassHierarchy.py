d553a61984f3afce5fed1b1a1c7fa4b5a6828f50
Gabriel Inocentes <gabriel.inocentes@gmail.com>
2026-04-23 21:38:03 -0400

    Added circle child class with required properties, setter, and area calculation, importing math library to have a more exact pi for the calculation.

--- shapesClassHierarchy/shapesClassHierarchy.py
+++ shapesClassHierarchy/shapesClassHierarchy.py
@@ -2,6 +2,7 @@
 
 # Abstract base class that holds the basic shape properties and methods
 from abc import ABC, abstractmethod
+import math
 
 # Abstract, or parent class that holds broad shapes
 class BasicShape(ABC):
@@ -40,3 +41,41 @@ class BasicShape(ABC):
     @abstractmethod
     def calc_area(self):
         pass
+
+    # A child class that inherits from the BasicShape class, and represents a circle, with properties for the center coordinates and radius, and a method to calculate the area of the circle
+    class Circle(BasicShape):
+        def __init__(self, x, y, r, n = "Circle"):
+            # Calls the __init__ method of the parent class to initialize the area and name properties
+            super().__init__() 
+            
+            self._x_center = x  
+            self._y_center = y
+            self._radius = r
+            self.name = n
+
+            # Calls the calc_area method to calculate the area of the circle when the object is created
+            self.calc_area() 
+
+        # Property for radius, x_center, and y_center that waits to see if .radius, .x_center, or .y_center is called, and if it is, returns the value of the radius, x_center, or y_center,
+        @property
+        def radius(self):
+            return self._radius
+
+        @property
+        def x_center(self):
+            return self._x_center
+
+        @property
+        def y_center(self):
+            return self._y_center
+
+        # Setter for radius, that also recalculates the area when called, and checks for valid input
+        @radius.setter
+        def radius(self, value):
+            if value >= 0:
+                self._radius = value
+                # Calls to calc_area to recalculate the area of the circle when the radius is changed
+                self.calc_area() 
+
+        def calc_area(self):
+            self._area = math.pi * self._radius ** 2
